22a518ea5a4d771e8e9879c7c964f732e30c38aa
Rahul121001 <139845999+Rahul121001@users.noreply.github.com>
2023-09-24 11:22:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-//your code here
-
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+function compareBands(a,b){
+	var bandA = a.replace(/^The /, "");
+	var bandB = b.replace(/^The /, "");
+	if(bandA < bandB){
+		return -1;
+	}
+	if(bandA > bandB){
+		return 1;
+	}
+	return 0;
+}
+bandNames.sort(compareBands);
+console.log(bandNames);
